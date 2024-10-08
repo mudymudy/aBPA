98bcee8383f9d069b3796b07b7eccd476bbd82e6
Bruno Romero <126069027+mudymudy@users.noreply.github.com>
2024-10-08 12:02:16 +0100

    Update plot_cvg_vs_completeness.py

--- bin/plot_cvg_vs_completeness.py
+++ bin/plot_cvg_vs_completeness.py
@@ -33,7 +33,7 @@ except ValueError as e:
 
 with open(data, 'r') as file:
         summary = pd.read_csv(data, sep='\t')
-        print("geneNormalizedSummary.tab file loaded successfully into python.")
+        print("geneNormalizedUpdated.tab file loaded successfully into python.")
         print(summary.head())
 
 max_value = summary['normalizedGeneSimple'].max()
@@ -43,7 +43,7 @@ line_segments = [
     [(0, coverage), (100, coverage)]  # Line 2: horizontal line at y=1.5
 ]
 
-plt.figure(figsize=[10,10])
+plt.figure(figsize=[15,10])
 ax = sns.scatterplot(data=summary, y='normalizedGeneSimple' , x='geneCompleteness' , hue='sampleID', s=7)
 
 lines = LineCollection(line_segments, linewidths=2, colors='black', linestyle='dashed')
@@ -51,14 +51,13 @@ ax.add_collection(lines)
 
 
 # Create the Rectangle patch
-rec1 = Rectangle((0, 0), 50, 12, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
+rec1 = Rectangle((0, 0), 50, max_value, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
 rec2 = Rectangle((50, 0), 50, 0.5, linewidth=1, edgecolor='none', facecolor='grey', alpha=0.3)
 
 # Add the rectangle to the plot
 ax.add_patch(rec1)
 ax.add_patch(rec2)
+plt.legend(markerscale=1, fontsize=10, loc='center right', bbox_to_anchor=(1.25,0.969))
 
-plt.legend(markerscale=1, fontsize=10, loc='upper left', bbox_to_anchor=(-0.01,1.1))
 
-
-plt.savefig('plotCoverage_vs_Completeness.png') 
+plt.savefig('plotCoverage_vs_Completeness.png', bbox_inches='tight')
